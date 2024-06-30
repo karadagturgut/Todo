72746d5878ba0a3c2e9d33e3cf03280b8109dd43
Turgut Karadağ <turgut_kara_dag@hotmail.com>
2024-06-30 05:34:25 +0300

    UserBoard tablosu eklendi.

--- Todo.Data/Context/TodoContext.cs
+++ Todo.Data/Context/TodoContext.cs
@@ -5,12 +5,13 @@ using Todo.Core;
 
 namespace Todo.Data
 {
-    public class TodoContext : IdentityDbContext<TodoUser,TodoRole,int>
+    public class TodoContext : IdentityDbContext<TodoUser, TodoRole, int>
     {
         public virtual DbSet<Assignments> Assigments { get; set; }
         public virtual DbSet<AssignmentStatus> AssignmentStatus { get; set; }
         public virtual DbSet<Boards> Boards { get; set; }
         public DbSet<ActionRoles> ActionRoles { get; set; }
+        public DbSet<UserBoard> UserBoards { get; set; }
         public TodoContext(DbContextOptions<TodoContext> options) : base(options)
         {
         }
---
+++ Todo.Data/Migrations/20240630022919_UserBoardsAdd.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todo.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserBoardsAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserBoards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BoardId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserBoards", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserBoards");
+        }
+    }
+}
